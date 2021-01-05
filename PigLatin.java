6ad80331e343de5be085b9ee6cd06b25101a78f9
vianalin <vlin21@stuy.edu>
2021-01-05 00:02:52 -0500

    pig latin 3

--- PigLatin.java
+++ PigLatin.java
@@ -32,6 +32,19 @@ public class PigLatin {
 			"st", "sw", "th", "tr", "tw", "wh", "wr"}));
 		if(s.length() > 1 && digraphs.contains(s.substring(0,2))) return s.substring(2) + s.substring(0,2) + "ay";
 		return pigLatinSimple(s);
-
 	}
+
+	//pig latin 3
+	public static String pigLatinBest(String s) {
+		//given a single word of at least 1 character, that can optionally have a punctuation mark afterwards:
+		//if word starts with nonletter, leave it alone
+		//else, apply same rules as piglatin() except now there can be a punctuation at the end of a word
+		//punctuation must remain after the word after you convert to pig latin
+		//output should be lower case to avoid issues with capitalization
+		s = s.toLowerCase();
+		if (!Character.isLetter(s.charAt(0))) return s;
+    	if (!Character.isLetter(s.charAt(s.length() - 1))) return pigLatin(s.substring(0, s.length() - 1)) + s.charAt(s.length() - 1);
+    	return pigLatin(s);
+  }
 }
+
