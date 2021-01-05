a8137fcb202b204cc90bb03129fd071056c8d927
vianalin <vlin21@stuy.edu>
2021-01-04 22:02:05 -0500

    added comments to understand scanner better

--- MakeStars.java
+++ MakeStars.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 public class MakeStars {
 	public static void main(String[] args) {
-		Scanner all = new Scanner(System.in); 
+		Scanner all = new Scanner(System.in); //all text
 		while(all.hasNextLine()) {
-			Scanner line = new Scanner(all.nextLine()); 
+			Scanner line = new Scanner(all.nextLine()); //line of text
 			while(line.hasNext()) {
-				String word = line.next(); 
-				String stars = ""; 
-				for(int i = 0; i < word.length(); i++) { 
+				String word = line.next(); //word
+				String stars = ""; //empty string to add stars for each letter
+				for(int i = 0; i < word.length(); i++) { //adding * to stars for each amount of letters
 					stars += "*";
 				}
-				System.out.println(stars + " "); 
+				System.out.println(stars + " "); //printing * with a space
 			}
-			System.out.println(); 
+			System.out.println(); //printing new line after each line
 		}
 	}
 }
